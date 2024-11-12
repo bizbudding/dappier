28d5e5fd1b8e2d745b225b28a43b1a9bd3709f45
JiveDig <jivetheuniversal@gmail.com>
2024-11-12 15:03:45 -0500

    whoops missed ver constant

--- dappier.php
+++ dappier.php
@@ -92,7 +92,7 @@ final class Dappier_Plugin {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'DAPPIER_PLUGIN_VERSION' ) ) {
-			define( 'DAPPIER_PLUGIN_VERSION', '0.2.0' );
+			define( 'DAPPIER_PLUGIN_VERSION', '0.3.0' );
 		}
 
 		// Plugin Folder Path.
